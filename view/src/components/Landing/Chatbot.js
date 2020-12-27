4de0b891a40083fd722c139f0fa4020e6c2e287e
Joe Whelton <joe.whelton@mycit.ie>
2020-12-27 16:08:36 +0000

    Chatbot visual enhancements

--- view/src/components/Landing/Chatbot.js
+++ view/src/components/Landing/Chatbot.js
@@ -1,15 +1,24 @@
-import React, {useContext, useEffect, useState} from 'react';
-
+import React, {useEffect, useRef, useState} from 'react';
+import {format} from 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
 import axios from "axios";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
+function ChatMessage(type, msg){
+    this.type = type;
+    this.msg = msg;
+    this.dateTime = new Date();
+}
+
 const Chatbot = (props) => {
     require('../../styles/chatbot.css');
-    const [chatTranscript, setChatTranscript] = useState('Hi, how can I help you?');
+    const [chatLog, setChatLog] = useState([]);
     const [currentQuestion, setCurrentQuestion] = useState('');
     const [expanded, setExpanded] = useState(false);
     const [answerLoading, setAnswerLoading] = useState(false);
+
+    const textInput = useRef(null);
 
     const onQuestionChange = (e) => {
         setCurrentQuestion(e.target.value);
@@ -17,51 +26,92 @@ const Chatbot = (props) => {
 
     const toggleExpand = () => {
         setExpanded(!expanded);
+        textInput.current.focus();
+    }
+
+    useEffect(() => {
+        if(chatLog[chatLog.length-1] && chatLog[chatLog.length-1].type === 'Q'){
+            let q = chatLog[chatLog.length-1].msg;
+            axios
+                .post(apiUrl + 'chatbot', {question: q})
+                .then((data) => {
+                    console.log(data.data.res);
+                    setChatLog([
+                        ...chatLog,
+                        new ChatMessage('A', data.data.res)
+                    ]);
+                    let ta = document.getElementById('chatbotTextarea');
+                    ta.scrollTop = ta.scrollHeight;
+                })
+                .catch((error) => {
+                    console.log(error);
+                    setChatLog([
+                        ...chatLog,
+                        new ChatMessage('A', 'Error Loading Chatbot Response')
+                    ]);
+                })
+                .finally(() => {
+                    setAnswerLoading(false);
+                    setCurrentQuestion('');
+                    textInput.current.focus();
+                });
+        }
+    }, [chatLog]);
+
+    const handleOnKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            submitQuestion();
+        }
     }
 
     const submitQuestion = () => {
-        setAnswerLoading(true);
-        let q = currentQuestion;
-        let currentChat = chatTranscript + '\n\n' + q;
-        setChatTranscript(currentChat);
-
-        axios
-            .post(apiUrl + 'chatbot', {question: q})
-            .then((data) => {
-                console.log(data.data.res);
-                setChatTranscript(chatTranscript + '\n\n' + q + '\n\n' + data.data.res);
-                let ta = document.getElementById('chatbotTextarea');
-                ta.scrollTop = ta.scrollHeight;
-            })
-            .catch((error) => {
-                setChatTranscript(chatTranscript + '\n\nError Loading Chatbot Response');
-            })
-            .finally(() => {
-                setAnswerLoading(false);
-                setCurrentQuestion('');
-            });
+        if(currentQuestion !== '') {
+            setAnswerLoading(true);
+            setChatLog([
+                ...chatLog,
+                new ChatMessage('Q', currentQuestion)
+            ]);
+        }
     }
 
     return(
             <div className='chatbotFrame'>
                 <div className='chatbotHeader'
                      onClick={toggleExpand}
-                     >Chat to us!</div>
+                >
+                    Chat to us!
+                    <div>
+                        {expanded ? '-' : '+'}
+                    </div>
+                </div>
                 <div className='chatbotBody'
                     style={{display: expanded ? 'flex' : 'none'}}>
-                    <textarea
-                        id='chatbotTextarea'
-                        className='chatbotChat'
-                        value={chatTranscript}/>
+                    <div className='chatLog' id='chatbotTextarea'>
+                        {chatLog.map((ch, i) => {
+                            return (
+                                <div key={'c' + ch.type + i} className='chatTextHolder'>
+                                    <div key={ch.type + i}
+                                        className={ch.type === 'Q' ? 'chatText question' : 'chatText answer'}
+                                        title={format(ch.dateTime, 'HH:mm:ss dd-MM-yyyy')}
+                                    >
+                                        {ch.msg}
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
                     <div className='inputHolder'>
                         <input
                             name="question"
                             value={currentQuestion}
                             onChange={onQuestionChange}
                             disabled={answerLoading}
+                            onKeyDown={handleOnKeyDown}
+                            ref={textInput}
                         />
                         <button
                             onClick={submitQuestion}
+                            disabled={currentQuestion === ''}
                         >Go</button>
                     </div>
                 </div>
